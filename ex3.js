698b3e7e8cb72506ac9b4b8ae89bd0622c634c07
nclsmrts <nicolasisabela1412@gmail.com>
2024-05-03 10:45:11 -0300

    feat: exercicio 2

---
+++ ex3.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let number = prompt("digite um numero")
+
+if (number%2 == 0) {
+    console.log("par");
+}else{
+    console.log("impar");
+}
