9537e2523f6728ad10fff500b35043aae1b94abc
nclsmrts <nicolasisabela1412@gmail.com>
2024-05-08 14:43:58 -0300

    feat:exercicio 6 while

---
+++ Loop/ex6.js
@@ -0,0 +1,10 @@
+// 6. Imprimir números pares de 0 a 20 utilizando o loop while
+
+let number = 0
+
+while (number <=20) {
+    number++
+    if(number%2 == 0){
+        console.log(number)
+    }
+}
