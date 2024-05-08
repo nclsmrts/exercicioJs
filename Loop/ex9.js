2df43689bc8308fd1ec690a077960a21ffc29ac8
nclsmrts <nicolasisabela1412@gmail.com>
2024-05-08 16:16:13 -0300

    feat:exercicio 9 while

---
+++ Loop/ex9.js
@@ -0,0 +1,9 @@
+// Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let num = parseInt(prompt("Digite um número: "))
+
+while (num > 0) {
+    num --
+    console.log(num);
+}
